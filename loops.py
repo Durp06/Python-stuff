a66eaece49875ed35b192116e363dc3bdd1d8734
Durp06 <98856010+Durp06@users.noreply.github.com>
2023-04-11 08:38:42 -0400

    looping practice

---
+++ loops.py
@@ -0,0 +1,12 @@
+def looper():
+    loops = int(input("Enter the number of words: "))
+    int i=0
+    words = []
+    
+    while (i<=loops):
+        toAdd = input("Enter word ", i, ": ")
+        words.append(toAdd)
+        i+=1
+
+    for word in words:
+        print(words[word])
